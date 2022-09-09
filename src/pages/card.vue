2376a618911ad54b3ac151f6e4138ea4b0a7a4c0
Steib-Audy-Victoria <95017413+Steib-Audy-Victoria@users.noreply.github.com>
2022-09-09 08:07:36 +0200

    Base

---
+++ src/pages/card.vue
@@ -0,0 +1,4 @@
+<script setup lang="ts">
+    import bath from "./icones/bath.vue"
+    
+</script>
---
+++ src/pages/simple.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+    import Bath from "../components/Bath"
+</script>
+<template>
+    <div class="p-2">
+        <h2>page simple</h2>
+    </div>
+</template>
