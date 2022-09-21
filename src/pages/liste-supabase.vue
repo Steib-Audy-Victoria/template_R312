187aec59a628af6d00c2193d0cd76f4933f9b544
Steib-Audy-Victoria <95017413+Steib-Audy-Victoria@users.noreply.github.com>
2022-09-21 16:55:15 +0200

    intégration supabase

---
+++ src/pages/liste-supabase.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import { supabase } from "../supabase";
+console.log("supabase :", supabase); // pour vérifier et "garder" supabase dans le code
+const maisons = []; // à remplacer par l'appel à Supabase
+</script>
+<template>
+    <h2>page liste - supabase</h2>
+    <Card v-for="maison in maisons" :key="maison.nom" v-bind="maison" />
+</template>
